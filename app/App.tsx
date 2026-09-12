2f8781f5350ed171f32bbbbd004c8b1c4bf83629
Anto-glitch <antmalecki@gmail.com>
2026-09-12 22:24:32 +0200

    precommit hook test

--- app/App.tsx
+++ app/App.tsx
@@ -3,11 +3,12 @@ import { Host, Checkbox } from '@expo/ui/jetpack-compose';
 import { useState } from 'react';
 
 export default function App() {
-  const [checked, setChecked] = useState(false); 
-   return (
-            <View className="flex-1 items-center justify-center bg-white">
-      <Host matchContents> <Checkbox value={checked} onCheckedChange={setChecked} />
-  </Host>
+  const [checked, setChecked] = useState(false);
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Host matchContents>
+        <Checkbox value={checked} onCheckedChange={setChecked} />
+      </Host>
       <Text className="text-xl font-bold text-blue-500">Welcome to Nativewind!</Text>
     </View>
   );
